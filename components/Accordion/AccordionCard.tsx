7f18c3459f82271fc9a4aa8850c1ee0ffa66965b
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-08 12:58:58 +0200

    Style auto adjust description height

--- components/Accordion/AccordionCard.tsx
+++ components/Accordion/AccordionCard.tsx
@@ -1,62 +1,62 @@
 import React, { useState } from 'react'
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md'
-import { MenuItems } from '../../pages/carta';
-import { AccordionContent, AccordionContentDescription, AccordionContentTitle, AccordionItem, AccordionTitle, ArrowControls } from './Accordion.styled';
+import { MenuItems } from '../../pages/carta'
+import {
+	AccordionContent,
+	AccordionContentDescription,
+	AccordionContentTitle,
+	AccordionItem,
+	AccordionTitle,
+	ArrowControls,
+} from './Accordion.styled'
 
 interface AccordionCardProps {
-  categoryTitle: string;
-  data: MenuItems[]
+	categoryTitle: string
+	data: MenuItems[]
 }
 
+const AccordionCard = ({ categoryTitle, data }: AccordionCardProps) => {
+	const [active, setActive] = useState(false)
 
-const AccordionCard = ({categoryTitle, data}: AccordionCardProps) => {
-  const [active, setActive] = useState(false);
+	const toggleAccordion = () => {
+		setActive((prevState) => !prevState)
+	}
 
-  const toggleAccordion = () => {
-    setActive(prevState => !prevState);
-  };
+	return (
+		<AccordionItem id={`${categoryTitle}`} onClick={toggleAccordion}>
+			<AccordionTitle className="accordionTitle" href={`#${categoryTitle}`}>
+				{categoryTitle}
+				<ArrowControls>
+					{active ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}
+				</ArrowControls>
+			</AccordionTitle>
 
-  return (
-      <AccordionItem id={`${categoryTitle}`} onClick={toggleAccordion}>
-        <AccordionTitle className='accordionTitle' href={`#${categoryTitle}`}>
-          {categoryTitle}
-          <ArrowControls>
-            {active ? <MdKeyboardArrowDown/> : <MdKeyboardArrowUp />}
-          </ArrowControls>
-        </AccordionTitle>
-
-        {data?.map(({name, description, price, id}) => (
-          <AccordionContent
-          key={id} 
-          style={{ maxHeight: active ? '12em' : '0' }}
-          >
-            <AccordionContentTitle>
-              <span className='contentItemName'>
-                <h3>{name}</h3>
-              </span>
-              <span className='contentPrice'>
-                <h3>{price}</h3>
-              </span>
-            </AccordionContentTitle>
-            <AccordionContentDescription>
-              <p>
-                {description}
-              </p>
-            </AccordionContentDescription>
-          </AccordionContent>
-        ))}
-
-      </AccordionItem>
-  )
+			{data?.map(({ name, description, price, id }) => (
+				<AccordionContent key={id} style={{ maxHeight: active ? '100%' : '0' }}>
+					<AccordionContentTitle>
+						<span className="contentItemName">
+							<h3>{name}</h3>
+						</span>
+						<span className="contentPrice">
+							<h3>{price}</h3>
+						</span>
+					</AccordionContentTitle>
+					<AccordionContentDescription>
+						<p>{description}</p>
+					</AccordionContentDescription>
+				</AccordionContent>
+			))}
+		</AccordionItem>
+	)
 }
 
 AccordionCard.defaultProps = {
-  categoryTitle: 'Category Name',
-  data: {
-  name: 'Menu Item Name',
-  price: 10,
-  description: 'Place Description Here',
-  }
+	categoryTitle: 'Category Name',
+	data: {
+		name: 'Menu Item Name',
+		price: 10,
+		description: 'Place Description Here',
+	},
 }
 
-export default AccordionCard;
+export default AccordionCard
