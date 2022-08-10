da0baba2d510c0f46a464460e0e203928d68cbf3
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-10 19:17:30 +0200

    Style change hours in menu

--- pages/carta.tsx
+++ pages/carta.tsx
@@ -32,7 +32,7 @@ const carta = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
 				<SubtitleCartaText>
 					Pedidos para llevar, domicilio y reserva de mesas al 624.41.45.18
 					<br></br>
-					Glovo y Just Eat disponible - Horario de 19:30 a 00:45 (Los Martes
+					Glovo y Just Eat disponible - Horario de 19:00 a 00:45 (Los Martes
 					cerrado)
 					<br></br>
 					Síguenos en Instagram: ringobar_
