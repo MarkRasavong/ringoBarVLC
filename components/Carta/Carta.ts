7a509cb5d689a19bca1a66f4368b74a2b652232d
Mark Rasavong <rasavong.mark@gmail.com>
2023-02-10 17:01:26 +0100

    Modify: cross compatibility for Carta.styled.ts

--- components/Carta/Carta.ts
+++ components/Carta/Carta.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 
 export const MenuContainer = styled.div`
+	display: -webkit-box;
+	display: -ms-flexbox;
 	display: flex;
+	-webkit-box-orient: vertical;
+	-webkit-box-direction: normal;
+	-ms-flex-direction: column;
 	flex-direction: column;
+	-webkit-box-align: center;
+	-ms-flex-align: center;
 	align-items: center;
 	min-height: 41em;
 	h1 {
