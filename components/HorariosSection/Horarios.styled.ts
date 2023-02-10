0f4137e8caea11a626f75712027b5a97c2d70ada
Mark Rasavong <rasavong.mark@gmail.com>
2023-02-10 16:46:09 +0100

    Modify: cross compatibility for Horarios.styled.ts

--- components/HorariosSection/Horarios.styled.ts
+++ components/HorariosSection/Horarios.styled.ts
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
 export const HorariosGrid = styled.div`
+	display: -ms-grid;
 	display: grid;
+	-ms-grid-columns: auto 60%;
 	grid-template-columns: auto 60%;
 	grid-row-gap: 0.6em;
+	-webkit-box-pack: center;
+	-ms-flex-pack: center;
 	justify-content: center;
+	-webkit-box-align: center;
+	-ms-flex-align: center;
 	align-items: center;
 	padding-top: 2em;
 	margin-left: 15%;
@@ -14,6 +20,8 @@ export const HorariosGrid = styled.div`
 	}
 
 	@media (min-width: 901px) {
+		-webkit-box-pack: center;
+		-ms-flex-pack: center;
 		justify-content: center;
 		margin-left: 20%;
 		padding-bottom: 4em;
