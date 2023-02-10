fd06ce06b26473cb6bd4d905d886af78f42c61b8
Mark Rasavong <rasavong.mark@gmail.com>
2023-02-10 16:57:40 +0100

    Modify: cross compatibility for Footer.styled.ts

--- components/Footer/Footer.styled.ts
+++ components/Footer/Footer.styled.ts
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 export const FooterDiv = styled.footer`
 	position: relative;
 	bottom: 0;
+	display: -webkit-box;
+	display: -ms-flexbox;
 	display: flex;
+	-webkit-box-pack: center;
+	-ms-flex-pack: center;
 	justify-content: center;
 	height: 5em;
 	width: 100%;
