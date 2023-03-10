f559afc4dfc3fd0cef5ffbfa459e6f1f0af70f61
Mark Rasavong <rasavong.mark@gmail.com>
2023-03-10 17:18:58 +0100

    Add: connect redux to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,22 @@ import GlobalStyles from '../styles/globals'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
 import Layout from '../components/Layout'
+import { Provider } from 'react-redux'
+import { store } from '../redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-  <Fragment>
-    <GlobalStyles />
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
-  </Fragment>
-  )
+	return (
+		<Fragment>
+			<GlobalStyles />
+			<ThemeProvider theme={theme}>
+				<Provider store={store}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</Provider>
+			</ThemeProvider>
+		</Fragment>
+	)
 }
 
 export default MyApp
