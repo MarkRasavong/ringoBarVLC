0153f9acd879acf058e3adff40c068e7c2a89688
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-15 14:50:14 +0200

    Add: Display nav and footer for order menu

--- pages/order.tsx
+++ pages/order.tsx
@@ -4,13 +4,20 @@ import React from 'react'
 const Order = () => {
 	return (
 		<div>
-			<div id="flipdish-menu" data-offset="0" data-restaurant="fd25971"></div>
 			<Script
 				id="flipdish-script"
 				type="text/javascript"
 				charSet="UTF-8"
 				src="https://web-order.flipdish.co/client/productionwlbuild/latest/static/js/main.js"
 			></Script>
+			<div
+				id="flipdish-menu"
+				data-initial-screen="menu"
+				data-full-screen-on-mobile="false"
+				data-offset="100"
+				data-restaurant="fd25971"
+				style={{ maxHeight: '500px' }}
+			></div>
 		</div>
 	)
 }
